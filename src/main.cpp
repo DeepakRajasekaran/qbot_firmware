ef8542849bfd7bc4b73b3f8d16d81a82d504d2d8
DeepakRajasekaran <rajasekarand375@gmail.com>
2025-05-21 09:06:39 +0530

    fixes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include "motor.h"
 
 // Create two motor instances
-Motor leftMotor(INVERSE, A0, 6, 7, 3, 5); // Motor 1: pwm = a0, in1=6, in2=7, encA=3, encB=5
-Motor rightMotor(DIRECT, A1, 8, 9, 2, 4); // Motor 2: pwm = a1, in1=8, in2=9, encA=2, encB=4
+Motor leftMotor(DIRECT, A0, 6, 7, 3, 5); // Motor 1: pwm = a0, in1=6, in2=7, encA=3, encB=5
+Motor rightMotor(INVERSE, A1, 8, 9, 2, 4); // Motor 2: pwm = a1, in1=8, in2=9, encA=2, encB=4
 
 void setup() {
     Serial.begin(9600); // Initialize Serial communication
